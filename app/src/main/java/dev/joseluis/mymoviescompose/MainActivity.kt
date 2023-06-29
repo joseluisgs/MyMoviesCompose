7101828c152ac24525f70e8c370fc032476c8560
José Luis González <joseluisgonsan@gmail.com>
2023-06-29 16:25:12 +0200

    Modifiers

--- app/src/main/java/dev/joseluis/mymoviescompose/MainActivity.kt
+++ app/src/main/java/dev/joseluis/mymoviescompose/MainActivity.kt
@@ -2,9 +2,12 @@ package dev.joseluis.mymoviescompose
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,6 +15,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -61,6 +65,40 @@ fun MediaItem() {
         ) {
             // Aquí irán las dos images
         }
-        Text(text = "Title 1")
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+               .fillMaxWidth()
+               .background(MaterialTheme.colors.secondary)
+                .padding(16.dp)
+        ) {
+            Text(text = "Title 1")
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 400,
+    heightDp = 400,
+    uiMode = UI_MODE_NIGHT_YES
+)
+
+// Puedo usar modifier para modificar el composable
+// Crearlo genérico y luego usarlo en varios sitios personalizado
+@Composable
+fun ButtonText(modifier: Modifier = Modifier) {
+    // Cuidado con el orden!!!
+    Box(modifier = Modifier
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center) {
+        Text(text = "Button",
+            modifier = modifier
+                .clickable { /*TODO*/ }
+                .background(Color.Cyan)
+                .border(2.dp, Color.Blue)
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+
+        )
     }
 }

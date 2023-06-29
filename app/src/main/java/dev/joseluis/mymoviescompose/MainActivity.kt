b7a5a62a3726bb9243e18cfb644ed19a32a81a1f
José Luis González <joseluisgonsan@gmail.com>
2023-06-29 14:31:44 +0200

    Box

--- app/src/main/java/dev/joseluis/mymoviescompose/MainActivity.kt
+++ app/src/main/java/dev/joseluis/mymoviescompose/MainActivity.kt
@@ -4,12 +4,16 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import dev.joseluis.mymoviescompose.ui.theme.MyMoviesComposeTheme
 
@@ -24,23 +28,47 @@ class MainActivity : ComponentActivity() {
             MyMoviesComposeTheme {
                 // Una superficie es un composable que permite definir el color de fondo de la app
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                   MyMainScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 // Las funciones con Preview no aceptan parámetros
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    widthDp = 400,
+    heightDp = 200,
+    uiMode = UI_MODE_NIGHT_YES
+)
 @Composable
 fun DefaultPreview() {
     MyMoviesComposeTheme {
-        Greeting("Android")
+        MyMainScreen()
     }
 }
+
+@Composable
+fun MyMainScreen() {
+    // Box es un composable que permite definir un contenedor de elementos
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.LightGray),
+        contentAlignment = Alignment.Center
+    ) {
+        Greeting(name = "Pepe", modifier = Modifier.background(Color.Blue))
+        Greeting(name = "Compose",
+            modifier = Modifier.background(Color.Red).align(Alignment.BottomCenter)
+        )
+    }
+
+
+}
+
+// Greting es un composable que permite definir un texto
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!", modifier = modifier)
+}
+

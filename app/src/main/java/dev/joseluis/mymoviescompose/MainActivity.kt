594581c8a6c328ef1cc28685e95593044b013cd4
José Luis González <joseluisgonsan@gmail.com>
2023-06-29 17:17:07 +0200

    Imagenes e Iconos

--- app/src/main/java/dev/joseluis/mymoviescompose/MainActivity.kt
+++ app/src/main/java/dev/joseluis/mymoviescompose/MainActivity.kt
@@ -8,15 +8,24 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.PlayCircleOutline
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -28,6 +37,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
 import dev.joseluis.mymoviescompose.ui.theme.MyMoviesComposeTheme
 
 // Clase principal de la app --> Actividad
@@ -74,7 +85,25 @@ fun MediaItem() {
         ) {
             // Aquí irán las dos images
             // Imagen con Coil
-            AsyncImage(model = "https://loremflickr.com/400/400/cat?lock=1" , contentDescription = "Image 1")
+            AsyncImage(model = "https://loremflickr.com/400/400/cat?lock=1", contentDescription = "Image 1",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop,
+            )
+            Icon(imageVector = Icons.Default.PlayCircleOutline, contentDescription = "Play", tint = Color.White.copy(alpha = 0.6f),
+                modifier = Modifier
+                    .size(120.dp)
+                    //.clip(RoundedCornerShape(92.dp))
+                    //.background(Color.Black.copy(alpha = 0.5f))
+                    .align(Alignment.Center)
+            )
+            // Iconos desde recursos
+            /*Icon(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "Play", tint = Color.White,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(RoundedCornerShape(92.dp))
+                    .background(Color.Black.copy(alpha = 0.5f))
+                    .align(Alignment.Center)
+            )*/
         }
         Box(
             contentAlignment = Alignment.Center,
@@ -85,6 +114,7 @@ fun MediaItem() {
         ) {
             Text(text = "Title 1",
                 fontSize = MaterialTheme.typography.h6.fontSize,
+                fontWeight = MaterialTheme.typography.h6.fontWeight,
             )
         }
     }
